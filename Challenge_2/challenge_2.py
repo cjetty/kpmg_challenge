66d4fe401d6de41792dd7236b1a5ded0a6f7cf8a
chandrasekhar <chandra.jetty09@gmail.com>
2022-02-05 19:44:48 +0000

    updated challenge2

--- Challenge_2/challenge_2.py
+++ Challenge_2/challenge_2.py
@@ -19,32 +19,12 @@ def get_instance_meta_data(meta_link, result, meta_data_key_recursive=None):
 
 if __name__ == '__main__':
     aws_meta_link = "http://169.254.169.254/latest/meta-data/"
-    valid_meta_data_keys = [u'scheduled', u'domain', u'configured', u'vpc-ipv4-cidr-blocks', u'vhostmd',
-                            u'security-group-ids', u'instance-type', u'subnet-ipv4-cidr-block', u'instance-id',
-                            u'local-hostname', u'availability-zone-id', u'public-ipv4s', u'hostname', u'ami-id',
-                            u'instance-action', u'interface-id', u'vpc-id', u'ami', u'reservation-id',
-                            u'security-groups', u'instance-life-cycle', u'subnet-id', u'mac', u'profile',
-                            u'public-ipv4', u'owner-id', u'ami-manifest-path', u'signer-cert', u'vpc-ipv4-cidr-block',
-                            u'info', u'local-ipv4', u'ami-launch-index', u'public-hostname', u'region', u'partition',
-                            u'availability-zone', u'ec2-instance', u'local-ipv4s', u'root', u'device-number', u'history'
-                            ]
-    meta_data_key = None
     parser = OptionParser()
     parser.add_option("-k", "--key", dest="data_key", help="To get history of data", default=None)
-    (options, args) = parser.parse_args()
-    if options.data_key and options.data_key in valid_meta_data_keys:
-        meta_data_key = options.data_key
-    elif options.data_key:
-        print("Requested key is not valid, Valid keys are", valid_meta_data_keys)
-        exit()
-    else:
-        pass
-    instance_meta_data = {}
+    instance_meta_data = dict()
     instance_meta_data_fetched = get_instance_meta_data(aws_meta_link, result=instance_meta_data)
-    if meta_data_key:
-        print(instance_meta_data_fetched[meta_data_key])
+    (options, args) = parser.parse_args()
+    if options.data_key:
+        print(instance_meta_data_fetched.get(options.data_key))
     else:
         print(instance_meta_data_fetched)
-
-
-
